c3afc9a71698bec3730bc2ec57326a46c5b3a9e4
PerishCode <perishcode@gmail.com>
2021-05-05 23:13:08 +0800

    MySQL 完结撒花

---
+++ mysql-hello/sql/index_test.sql
@@ -0,0 +1,14 @@
+use hello;
+
+-- create unique index id_index on index_test(id);
+-- alter table
+--     index_test
+-- add
+--     index first_name_index(first_name);
+-- alter table
+--     index_test
+-- add
+--     primary key (id);
+-- drop index id_index on index_test;
+-- alter table
+--     index_test drop primary key;
--- mysql-hello/sql/table-copy.sql
+++ mysql-hello/sql/table-copy.sql
@@ -1,16 +1,27 @@
 use hello;
 
-create table if not exists emp_simple_copy (id int, `name` varchar(32));
+-- create table if not exists emp_simple_copy (id int, `name` varchar(32));
+-- insert into
+--     emp_simple_copy (id, `name`)
+-- select
+--     empno,
+--     ename
+-- from
+--     emp;
+-- select
+--     *
+-- from
+--     emp_simple_copy;
+create table if not exists emp_complete_copy like emp;
 
 insert into
-    emp_simple_copy (id, `name`)
+    emp_complete_copy
 select
-    empno,
-    ename
+    *
 from
     emp;
 
 select
     *
 from
-    emp_simple_copy;
+    emp_complete_copy;
