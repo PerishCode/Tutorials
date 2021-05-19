3a19411afcf0519407a0c5cb8178c2a3f6046c6c
PerishCode <perishcode@gmail.com>
2021-05-19 14:28:47 +0800

    nothing

--- vdom-hello/src/main.ts
+++ vdom-hello/src/main.ts
@@ -19,34 +19,45 @@ const app = document.getElementById('app')!
 
 const key = 'a'
 
+// observe((operation: any = {}) => {
+//   const { type } = operation
+
+//   const target = source[key]
+
+//   console.log(operation)
+
+//   switch (type) {
+//     case 'set': {
+//       const elementMap = reaction2elementMap.get(source)!
+//       const element = elementMap.get(key)!
+//       element.innerHTML = target.text
+//     }
+//     case 'delete': {
+//       const elementMap = reaction2elementMap.get(source)!
+//       const element = elementMap.get(key)!
+//       element.remove()
+//     }
+
+//     case 'add':
+//     case undefined: {
+//       if (target === undefined) return
+
+// const elementMap = reaction2elementMap.acquire(source, new MapExtension())
+// const element = elementMap.acquire(key, document.createElement('div'))
+
+// element.innerHTML = target.text
+// app.appendChild(element)
+//     }
+//   }
+// })
+
 observe((operation: any = {}) => {
   const { type } = operation
-
-  const target = source[key]
-
-  console.log(operation)
+  const target = source['a']
 
   switch (type) {
-    case 'set': {
-      const elementMap = reaction2elementMap.get(source)!
-      const element = elementMap.get(key)!
-      element.innerHTML = target.text
-    }
-    case 'delete': {
-      const elementMap = reaction2elementMap.get(source)!
-      const element = elementMap.get(key)!
-      element.remove()
-    }
-
-    case 'add':
     case undefined: {
-      if (target === undefined) return
-
-      const elementMap = reaction2elementMap.acquire(source, new MapExtension())
-      const element = elementMap.acquire(key, document.createElement('div'))
-
-      element.innerHTML = target.text
-      app.appendChild(element)
+      observe(() => {})
     }
   }
 })
